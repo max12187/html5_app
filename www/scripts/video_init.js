d1bf81b9768275dc205408bded6f63242a95cd2f
Maxwell Lemkin <ml324871@wne.edu>
2014-06-19 12:49:52 -0400

    fixed the resizing issue

--- www/scripts/video_init.js
+++ www/scripts/video_init.js
@@ -125,13 +125,14 @@ $(document).ready(function(){
 	
 	
 	//now lets dynamically resize the elements on the page to make them fit
-	var height = $(document).height() - 50;
-	var titleHeight = $("#title").height();
+	var height = $('body').height() - $('header').height();
+	var titleHeight = $("#title").height() + 10;
+	console.log(height);
 	console.log(titleHeight);
-
+	console.log(height - titleHeight);
 	
 
-	$('#video').css({'height':height - (2*titleHeight)});
+	$('#video').css({'height':height - titleHeight});
 	
 	
 	
